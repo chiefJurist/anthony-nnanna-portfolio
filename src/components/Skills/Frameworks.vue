1b1b2607df77f34ef25f2d4f66c0affa515f61a0
chiefJurist <anthonynnanna004@gmail.com>
2024-02-25 01:47:13 +0100

    update

--- src/components/Skills/Frameworks.vue
+++ src/components/Skills/Frameworks.vue
@@ -25,6 +25,18 @@
 
     <div>
         <div class="sub-skill-header">
+            <span>Tailwind</span>
+            <span class="sub-skill-percentage">100%</span>
+        </div>
+        <div class="progress-bar-con">
+            <div class="progress-bar">
+                <div style="width: 100%;" class="progress"></div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="sub-skill-header">
             <span>Nuxt.js</span>
             <span class="sub-skill-percentage">90%</span>
         </div>
