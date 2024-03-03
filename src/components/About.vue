e1c8e51edb62ed3985e011fdb7a495241f1346ee
chiefJurist <anthonynnanna004@gmail.com>
2024-03-03 21:19:13 +0100

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,8 +4,8 @@
             return {
                 imageVisible: false, // Add a new property to track image visibility
                 classApplied: false, // Add a flag to track whether the class has been applied or not
-                divVisible: false,
-                divClassApplied: false 
+                pointVisible: false,
+                pointClassApplied: false
             };
         },
         mounted() {
@@ -35,19 +35,19 @@
                 }
 
                 // Check if the class has been applied already
-                if (!this.divClassApplied) {
-                    // Check for div visibility
-                    const div = document.getElementById('about-div');
-                    const divRect = div.getBoundingClientRect();
-                    if (divRect.top < window.innerHeight && divRect.bottom >= 0) {
-                        this.divVisible = true;
+                if (!this.pointClassApplied) {
+                    // Check for point visibility
+                    const points = document.getElementById('about-points');
+                    const pointRect = points.getBoundingClientRect();
+                    if (pointRect.top < window.innerHeight && pointRect.bottom >= 0) {
+                        this.pointVisible = true;
                     } else {
-                        this.divVisible = false;
+                        this.pointVisible = false;
                     }
 
-                    // If div is visible, apply the class and update the flag
-                    if (this.divVisible) {
-                        this.divClassApplied = true;
+                    // If point is visible, apply the class and update the flag
+                    if (this.pointVisible) {
+                        this.pointClassApplied = true;
                     }
                 }
             }
@@ -72,20 +72,20 @@
                 </div>
                 
                 <!--Details-->
-                <div class="pt-4 w-fit mx-auto" id="about-div" :class="{ 'animate-right': divVisible }">
-                    <div class="text-gray-500 dark:text-white text-lg max-w-96 font-semibold">
+                <div class="pt-4 w-fit mx-auto">
+                    <div class="text-gray-500 dark:text-white text-lg max-w-96" id="about-div">
                         I possess a self-motivated attitude, thriving in dynamic environments, and am also deeply passionate about learning new skills that facilitate my growth and development. Additionally, my proficiency in leadership and collaboration within teams enables me to excel collectively. Currently, I am actively seeking a competitive position to further enhance my skills
                     </div>
-                    <div class=" max-w-96 flex justify-around gap-5 text-gray-500 dark:text-white font-semibold pt-8 text-center">
-                        <div class="about-points">
+                    <div id="about-points" class=" max-w-96 flex justify-around gap-5 text-gray-500 dark:text-white font-semibold pt-8 text-center">
+                        <div :class="{ 'animate-right': pointVisible }">
                             <div class=" text-2xl text-black dark:text-white font-extrabold">4.75</div>
                             <div class=" text-wrap">Aggregrate CGPA</div>
                         </div>
-                        <div class="about-points">
+                        <div :class="{ 'animate-grow': pointVisible }">
                             <div class=" text-2xl text-black dark:text-white font-extrabold">30+</div>
                             <div>Web Development Projects</div>
                         </div>
-                        <div class="about-points">
+                        <div :class="{ 'animate-left': pointVisible }">
                             <div class=" text-2xl text-black dark:text-white font-extrabold">4+</div>
                             <div>Years Experience</div>
                         </div>
