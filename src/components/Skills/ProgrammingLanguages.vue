3b0b786521efb32a3a14bd1e3755559c2be16e18
chiefJurist <anthonynnanna004@gmail.com>
2024-03-09 20:34:28 +0100

    update

--- src/components/Skills/ProgrammingLanguages.vue
+++ src/components/Skills/ProgrammingLanguages.vue
@@ -25,6 +25,18 @@
 
     <div>
         <div class="sub-skill-header">
+            <span>Node.js</span>
+            <span class="sub-skill-percentage">100%</span>
+        </div>
+        <div class="progress-bar-con">
+            <div class="progress-bar">
+                <div style="width: 100%;" class="progress"></div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="sub-skill-header">
             <span>TypeScript</span>
             <span class="sub-skill-percentage">100%</span>
         </div>
