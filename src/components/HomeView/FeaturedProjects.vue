93a41ae4b3ad0be8ce4498cacd5ce20cb3731f6d
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-11-07 07:18:14 +0100

    edit

--- src/components/HomeView/FeaturedProjects.vue
+++ src/components/HomeView/FeaturedProjects.vue
@@ -96,15 +96,15 @@ export default {
                         class="w-full h-52 object-cover"
                     />
                     <div class="p-6">
-                        <h3 class="text-2xl font-bold text-black dark:text-white mb-2">
+                        <div class="text-2xl font-bold text-black dark:text-white mb-2">
                             {{ project.title }}
-                        </h3>
-                        <p class="text-indigo-600 dark:text-indigo-400 font-medium mb-2">
+                        </div>
+                        <div class="text-indigo-600 dark:text-indigo-400 font-medium mb-2">
                             {{ project.subtitle }}
-                        </p>
-                        <p class="text-gray-700 dark:text-gray-300 mb-4">
+                        </div>
+                        <div class="text-gray-700 dark:text-gray-300 mb-4">
                             {{ project.description }}
-                        </p>
+                        </div>
 
                         <div class="flex flex-wrap gap-2 mb-4">
                             <span
